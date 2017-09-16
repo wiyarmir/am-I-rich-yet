48d97dd94e823a11f1d351a6314100e5666c6bc9
Guillermo Orellana <wiyarmir@gmail.com>
2017-09-16 15:11:16 +0100

    test structure

---
+++ services/src/test/java/es/guillermoorellana/amirichyet/`test of a test`.kt
@@ -0,0 +1,15 @@
+package es.guillermoorellana.amirichyet
+
+import org.junit.Before
+import org.junit.Test
+
+class `test of a test` {
+    @Before
+    fun before() {
+    }
+
+    @Test
+    fun `I know how to configure tests`() {
+        assert(1 == 1)
+    }
+}
